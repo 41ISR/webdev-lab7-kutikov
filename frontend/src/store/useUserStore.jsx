f0de70634664d6789b7b10fec85c352a6009a29a
Pasha <39154112+ktkv419@users.noreply.github.com>
2025-11-18 05:54:13 +0000

    123

--- frontend/src/store/useUserStore.jsx
+++ frontend/src/store/useUserStore.jsx
@@ -2,14 +2,15 @@ import { create } from 'zustand'
 import { persist, createJSONStorage } from 'zustand/middleware'
 
 export const useUserStore = create()(
-  persist(
-    (set, get) => ({
-      session: undefined,
-      setSession: (session) => set({ ...session }),
+    persist(
+        (set, get) => ({
+            session: undefined,
+            setSession: (session) => set({ ...session }),
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!
     }),
-    {
-      name: 'user-storage', // name of the item in the storage (must be unique)
-      storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
+{
+    name: 'user-storage', // name of the item in the storage (must be unique)
+        storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
     },
   ),
 )
