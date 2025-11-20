3d9aebc604af2756c649c0d6d3efe1208b6a533d
Pasha <39154112+ktkv419@users.noreply.github.com>
2025-11-20 06:26:58 +0000

    123

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,19 +1,12 @@
 import { useEffect, useState } from "react"
 import { api } from "../api/api"
 import MessageCard from "./MessageCard"
+import { useMessageStore } from "../store/useMessageStore"
 
 const Feed = ({ title = "Сообщения" }) => {
-    const [messages, setMessages] = useState([])
+    const { messages, getMessages } = useMessageStore()
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const data = await api.getMessages()
-                setMessages(data)
-            } catch (error) {
-                console.error(error)
-            }
-        }
-        fetchData()
+        getMessages()
     }, [])
 
     return (
@@ -22,7 +15,7 @@ const Feed = ({ title = "Сообщения" }) => {
                 <div className="container">
                     <h2 className="section-title">{title}</h2>
                     <div className="messages-grid">
-                        {messages.map((el, i) => (
+                        {messages && messages.map((el, i) => (
                             <MessageCard key={i} {...el} />
                         ))}
                     </div>
